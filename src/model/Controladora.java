2c5b148862b290c65abe7d754e00a746bb1d2e7f
Nicolas Santiago Buelvas Gonzalez <1011089074@u.icesi.edu.co>
2024-11-15 11:30:55 -0500

    Envio 1

--- src/model/Controladora.java
+++ src/model/Controladora.java
@@ -10,7 +10,7 @@ public class Controladora {
      * Constructor de la clase Controladora para inicializar
      *
      * @pre No se requieren precondiciones específicas.
-     * @post Se crea una instancia de Controladora 
+     * @post Se crea una instancia de Controladora
      */
     public Controladora() {
         tableroTresEnRaya = new String[3][3];
@@ -55,5 +55,56 @@ public class Controladora {
             j = rand.nextInt(3);
         } while (!tableroTresEnRaya[i][j].equals(" "));
         tableroTresEnRaya[i][j] = "X";
+    }
+
+    /**
+     * Permite al jugador humano realizar una jugada.
+     *
+     * @param fila    la fila donde desea jugar (0-2)
+     * @param columna la columna donde desea jugar (0-2)
+     * @return true si la jugada es válida y se realiza, false si no es válida.
+     */
+    public boolean realizarJugadaHumano(int fila, int columna) {
+        if (fila >= 0 && fila < 3 && columna >= 0 && columna < 3 && tableroTresEnRaya[fila][columna].equals(" ")) {
+            tableroTresEnRaya[fila][columna] = "O";
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Verifica si hay un ganador en el tablero.
+     *
+     * @return "X" si gana la máquina, "O" si gana el humano, o null si no hay ganador.
+     */
+    public String verificarGanador() {
+        for (int i = 0; i < 3; i++) {
+            if (tableroTresEnRaya[i][0].equals(tableroTresEnRaya[i][1]) &&
+                tableroTresEnRaya[i][1].equals(tableroTresEnRaya[i][2]) &&
+                !tableroTresEnRaya[i][0].equals(" ")) {
+                return tableroTresEnRaya[i][0];
+            }
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (tableroTresEnRaya[0][j].equals(tableroTresEnRaya[1][j]) &&
+                tableroTresEnRaya[1][j].equals(tableroTresEnRaya[2][j]) &&
+                !tableroTresEnRaya[0][j].equals(" ")) {
+                return tableroTresEnRaya[0][j];
+            }
+        }
+
+        if (tableroTresEnRaya[0][0].equals(tableroTresEnRaya[1][1]) &&
+            tableroTresEnRaya[1][1].equals(tableroTresEnRaya[2][2]) &&
+            !tableroTresEnRaya[0][0].equals(" ")) {
+            return tableroTresEnRaya[0][0];
+        }
+        if (tableroTresEnRaya[0][2].equals(tableroTresEnRaya[1][1]) &&
+            tableroTresEnRaya[1][1].equals(tableroTresEnRaya[2][0]) &&
+            !tableroTresEnRaya[0][2].equals(" ")) {
+            return tableroTresEnRaya[0][2];
+        }
+
+        return null;
     }
 }
--- src/ui/Executable.java
+++ src/ui/Executable.java
@@ -9,20 +9,30 @@ public class Executable {
     private Controladora cont;
     private static boolean flag;
 
+    /**
+     * Constructor de la clase Executable.
+     */
     private Executable() {
         reader = new Scanner(System.in);
         cont = new Controladora();
     }
 
+    /**
+     * Método principal que ejecuta el menú del programa.
+     *
+     * @param flag indica si el programa debe continuar ejecutándose.
+     */
     public void run(boolean flag) {
-
         flag = false;
 
         while (!flag) {
-
             System.out.println("\n\nBienvenido al menu:\n");
-            System.out.println("Opciones:\n" + "1. Imprimir tablero \n" + "2. Jugada de la máquina \n"
-                    + "3. Jugada de humano \n" + "4. Verificar ganador \n" + "5. Salir del programa \n");
+            System.out.println("Opciones:\n" +
+                    "1. Imprimir tablero \n" +
+                    "2. Jugada de la maquina \n" +
+                    "3. Jugada de humano \n" +
+                    "4. Verificar ganador \n" +
+                    "5. Salir del programa \n");
 
             int option = reader.nextInt();
             reader.nextLine();
@@ -45,34 +55,66 @@ public class Executable {
                     System.exit(0);
                     break;
                 default:
-                    System.out.print("Por favor ingrese una opcion valida");
+                    System.out.print("Por favor ingrese una opcion valida.");
                     continue;
             }
-
         }
-
     }
 
+    /**
+     * Método main que inicia la ejecución del programa.
+     *
+     * @param args argumentos del programa.
+     */
     public static void main(String[] args) {
         Executable mainApp = new Executable();
         mainApp.run(flag);
     }
 
+    /**
+     * Imprime el tablero actual en la consola.
+     */
     private void imprimirTablero() {
         System.out.println(cont.obtenerTableroComoString());
     }
 
+    /**
+     * Realiza una jugada de la máquina y actualiza el tablero.
+     */
     private void jugadaMaquina() {
         cont.jugadaAleatoria();
-        System.out.println("La máquina ha realizado su jugada.");
+        System.out.println("La maquina ha realizado su jugada.");
         imprimirTablero();
     }
 
+    /**
+     * Permite al jugador humano realizar una jugada ingresando las coordenadas.
+     */
     private void jugadaHumano() {
-        // Implementación de jugada de humano
+        System.out.println("Ingrese la fila (0-2) para su jugada: ");
+        int fila = reader.nextInt();
+        System.out.println("Ingrese la columna (0-2) para su jugada: ");
+        int columna = reader.nextInt();
+        reader.nextLine();
+
+        boolean jugadaExitosa = cont.realizarJugadaHumano(fila, columna);
+        if (jugadaExitosa) {
+            System.out.println("Jugada realizada con exito.");
+        } else {
+            System.out.println("Jugada invalida, intente nuevamente.");
+        }
+        imprimirTablero();
     }
 
+    /**
+     * Valida si hay un ganador en el tablero actual.
+     */
     private void validarGanador() {
-        // Implementación de la validación si alguien ya ganó el triqui
+        String ganador = cont.verificarGanador();
+        if (ganador != null) {
+            System.out.println("Tenemos un ganador! El ganador es: " + ganador);
+        } else {
+            System.out.println("No hay un ganador todavia.");
+        }
     }
 }
